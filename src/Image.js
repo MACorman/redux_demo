f423f4fb5b4e992944a9ad6316d3105afd0c53d0
MACorman <madelinecorman@gmail.com>
2020-04-21 16:44:24 -0400

    set up some initial components and random image generator

---
+++ src/Image.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Image() {
+    return (
+        <img src="https://picsum.photos/200/300" />
+    )
+}
+
+export default Image
---
+++ src/Likes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Likes extends React.Component {
+    render() {
+        return (
+            <div>likes</div>
+        )
+    }
+}
+
+export default Likes
